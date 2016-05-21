b10bd5c84bb86f3b0337382a1a1f559ccf254628
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-21 17:59:54 -0600

    created bonus file

---
+++ cheatsheet_bonus.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'pry'
+
+
+
+
+get '/' do 
+	erb :index
+end
+
+
+get '/cp' do 
+	erb :cp
+end
+
+
+get '/mv' do 
+	erb :mv
+end
+
+
+get '/touch' do 
+	erb :touch
+end
+
+
+get '/mkdir' do 
+	erb :mkdir
+end
+
+
+get '/ls' do 
+	erb :ls
+end
+
+
+not_found do
+	status 404
+	'not found'
+end
